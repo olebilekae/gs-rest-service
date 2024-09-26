10cfcfc1cf328acc11a4651c07c5ade2fea48243
Junior Moremong <junior@kaelekae.com>
2024-09-26 11:55:42 +0200

    changed port

--- complete/src/main/java/com/example/restservice/RestServiceApplication.java
+++ complete/src/main/java/com/example/restservice/RestServiceApplication.java
@@ -1,5 +1,7 @@
 package com.example.restservice;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestServiceApplication.class, args);
+		SpringApplication app = new SpringApplication(RestServiceApplication.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", "5555"));
+        app.run(args);
 	}
 
 }
