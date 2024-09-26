d6acbc42564b6cec1f4375d59ae503001834770e
Junior Moremong <junior@kaelekae.com>
2024-09-26 16:03:45 +0200

    changes for logging

--- initial/src/main/java/com/example/restservice/RestServiceApplication.java
+++ initial/src/main/java/com/example/restservice/RestServiceApplication.java
@@ -1,13 +1,24 @@
 package com.example.restservice;
 
+import java.util.Collections;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestServiceApplication {
 
+	final static Logger logger = LoggerFactory.getLogger(RestServiceApplication.class);
+
+
 	public static void main(String[] args) {
-		SpringApplication.run(RestServiceApplication.class, args);
+
+		logger.info("Entering application.");
+		SpringApplication app = new SpringApplication(RestServiceApplication.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", "5555"));
+        app.run(args);
 	}
 
 }
